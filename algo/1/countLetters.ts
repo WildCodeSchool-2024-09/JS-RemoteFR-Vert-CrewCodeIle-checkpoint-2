e72c59d55c1306ed536974dc0aec3b18268092a4
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2024-12-02 09:49:51 +0100

    algo1 fini

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -11,7 +11,15 @@ Exemples :
 
 function countLetters(givenString: string, letter: string): number {
   // Your code here !
-  return 0;
+  let count = 0;
+
+  for (let i = 0; i < givenString.length; i++) {
+    if (givenString[i] === letter) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 export default countLetters;
