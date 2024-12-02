b358e3381da8fd221f661ef52aadd36022be556b
Sanesthesie <victorsanlopez@gmail.com>
2024-12-02 10:48:52 +0100

    finished step2 step1

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -27,7 +27,7 @@ const router = createBrowserRouter([
       {
         path: "/cupcakes",
         element: <CupcakeList />,
-        // Step 1: load data here
+        loader: () => fetch(`${import.meta.env.VITE_API_URL}/api/cupcakes`),
       },
     ],
   },
