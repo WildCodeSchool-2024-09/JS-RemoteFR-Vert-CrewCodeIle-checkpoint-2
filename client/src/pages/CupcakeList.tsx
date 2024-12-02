ff40825e73ccf8fa91b5e0f14f91b24425b056e1
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2024-12-02 11:06:02 +0100

    step2 fini

--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -40,6 +40,7 @@ type CupcakeArray = typeof sampleCupcakes;
 
 function CupcakeList() {
   // Step 1: get all cupcakes
+
   console.info(useLoaderData() as CupcakeArray);
 
   // Step 3: get all accessories
@@ -62,9 +63,11 @@ function CupcakeList() {
       <ul className="cupcake-list" id="cupcake-list">
         {/* Step 2: repeat this block for each cupcake */}
         {/* Step 5: filter cupcakes before repeating */}
-        <li className="cupcake-item">
-          <Cupcake data={sampleCupcakes[0]} />
-        </li>
+        {sampleCupcakes.map((cupcake) => (
+          <li className="cupcake-item" key={cupcake.id}>
+            <Cupcake data={cupcake} />
+          </li>
+        ))}
         {/* end of block */}
       </ul>
     </>
