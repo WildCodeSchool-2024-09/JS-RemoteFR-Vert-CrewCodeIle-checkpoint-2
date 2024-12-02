b69049767ed169c7c208abff2477fdf1942ffc39
Sanesthesie <victorsanlopez@gmail.com>
2024-12-02 13:55:16 +0100

    finished step2 step5

--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -2,66 +2,52 @@ import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import Cupcake from "../components/Cupcake";
 
-/* ************************************************************************* */
-const sampleCupcakes = [
-  {
-    id: 10,
-    accessory_id: "4",
-    accessory: "wcs",
-    color1: "blue",
-    color2: "white",
-    color3: "red",
-    name: "France",
-  },
-  {
-    id: 11,
-    accessory_id: "4",
-    accessory: "wcs",
-    color1: "yellow",
-    color2: "red",
-    color3: "black",
-    name: "Germany",
-  },
-  {
-    id: 27,
-    accessory_id: "5",
-    accessory: "christmas-candy",
-    color1: "yellow",
-    color2: "blue",
-    color3: "blue",
-    name: "Sweden",
-  },
-];
-
-type CupcakeArray = typeof sampleCupcakes;
+type CupcakeArray = {
+  id: number;
+  accessory_id: string;
+  accessory: string;
+  color1: string;
+  color2: string;
+  color3: string;
+  name: string;
+};
 type AccessoryArray = { id: number; name: string; slug: string }[];
 
-/* you can use sampleCupcakes if you're stucked on step 1 */
-/* if you're fine with step 1, just ignore this ;) */
-/* ************************************************************************* */
-
 function CupcakeList() {
-  const cupcakeList = useLoaderData() as CupcakeArray;
-  console.info(useLoaderData() as CupcakeArray);
-
+  const cupcakeList = useLoaderData() as CupcakeArray[];
   const [accessories, setAccessories] = useState<AccessoryArray>([]);
+  const [selectedAccessory, setSelectedAccessory] = useState("");
+  const [filteredCupcakes, setFilteredCupcakes] = useState(cupcakeList);
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_API_URL}/api/accessories`)
       .then((response) => response.json())
       .then((data: AccessoryArray) => setAccessories(data));
   }, []);
-
-  // Step 5: create filter state
+  useEffect(() => {
+    if (selectedAccessory === "") {
+      setFilteredCupcakes(cupcakeList);
+    } else {
+      setFilteredCupcakes(
+        cupcakeList.filter(
+          (cupcake) => cupcake.accessory_id === selectedAccessory,
+        ),
+      );
+    }
+  }, [selectedAccessory, cupcakeList]);
 
   return (
     <>
       <h1>My cupcakes</h1>
       <form className="center">
         <label htmlFor="cupcake-select">
-          {/* Step 5: use a controlled component for select */}
           Filter by{" "}
-          <select id="cupcake-select">
+          <select
+            id="cupcake-select"
+            value={selectedAccessory}
+            onChange={(event) => setSelectedAccessory(event.target.value)}
+          >
+            <option value=""> </option>
             {accessories.map((accessory) => (
               <option value={accessory.id} key={accessory.id}>
                 {accessory.name}
@@ -71,12 +57,11 @@ function CupcakeList() {
         </label>
       </form>
       <ul className="cupcake-list" id="cupcake-list">
-        {cupcakeList.map((cupcake) => (
+        {filteredCupcakes.map((cupcake) => (
           <li className="cupcake-item" key={cupcake.id}>
             <Cupcake data={cupcake} />
           </li>
         ))}
-        {/* Step 5: filter cupcakes before repeating */}
       </ul>
     </>
   );
