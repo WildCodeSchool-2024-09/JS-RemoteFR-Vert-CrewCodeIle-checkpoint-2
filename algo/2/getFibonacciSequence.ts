8445626775875f51456ff02d406d457c43e2043f
MATHIEU FOUCAULT <foucault.mathieu.pro@gmail.com>
2024-12-02 09:49:33 +0100

    finished algo 2

--- algo/2/getFibonacciSequence.ts
+++ algo/2/getFibonacciSequence.ts
@@ -11,8 +11,16 @@ Si n est inférieur ou égal à 0, la fonction doit retourner un tableau vide []
 */
 
 function getFibonacciSequence(size: number): number[] {
-  // Your code here !
-  return [0, 1];
+  if (size <= 0) return [];
+  if (size === 1) return [0];
+
+  const fibonacci: number[] = [0, 1];
+
+  for (let i = 2; i < size; i++) {
+    fibonacci.push(fibonacci[i - 1] + fibonacci[i - 2]);
+  }
+
+  return fibonacci;
 }
 
 export default getFibonacciSequence;
