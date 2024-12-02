3764eb4e049a81e048eeef653629769237a930e8
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2024-12-02 10:32:28 +0100

    feat: add loader, step1

--- algo/2/getFibonacciSequence.ts
+++ algo/2/getFibonacciSequence.ts
@@ -10,9 +10,6 @@ Détails
 Si n est inférieur ou égal à 0, la fonction doit retourner un tableau vide []
 */
 
-function getFibonacciSequence(size: number): number[] {
-  // Your code here !
-  return [0, 1];
-}
+function getFibonacciSequence(size: number): number[] {}
 
 export default getFibonacciSequence;
